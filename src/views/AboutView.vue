8316d737e54c7ebaccab8b6109bfb761599cc489
Sam Sharff <samanthaleec@gmail.com>
2022-05-02 15:20:49 -0400

    adds cool message

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>This is a cool about page</h1>
-    Brandon and Reeti are cool
+    Brandon and Reeti are cool. So are Deana and Sam, yo.
   </div>
 </template>
