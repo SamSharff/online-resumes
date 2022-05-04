5620aa75f7cb89a5130124b713aaf2058fc1815c
Sam Sharff <samanthaleec@gmail.com>
2022-05-04 14:33:54 -0400

    updates student data and template to backend

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -6,44 +6,39 @@ export default {
     return {
       students: [
         {
-          firstName: "first name",
-          lastName: "last name",
-          email: "example@example.com",
-          // phoneNumber: "832",
-          shortBio: "bio",
-          linkedIn: "LIurl",
-          // twitterHandle: "Twitter",
-          // personalWebsite: "website",
-          // onlineResume: "resume",
-          github: "git",
-          // photo: "img",
+          firstName: "",
+          lastName: "",
+          email: "",
+          shortBio: "",
+          linkedIn: "",
+          github: "",
         },
       ],
-      educations: [
-        {
-          startDate: "start",
-          endDate: "end",
-          degree: "degree",
-          universityName: "uni",
-          details: "details",
-        },
-      ],
-      skills: ["Ruby", "HTML"],
-      experiences: [
-        {
-          jobTitle: "job",
-          startDate: "start",
-          endDate: "end",
-          companyName: "company",
-          details: "details",
-        },
-      ],
-      capstone: {
-        name: "name",
-        description: "desc",
-        url: "url",
-        screenshot: "img",
-      },
+      // educations: [
+      //   {
+      //     startDate: "start",
+      //     endDate: "end",
+      //     degree: "degree",
+      //     universityName: "uni",
+      //     details: "details",
+      //   },
+      // ],
+      // skills: ["Ruby", "HTML"],
+      // experiences: [
+      //   {
+      //     jobTitle: "job",
+      //     startDate: "start",
+      //     endDate: "end",
+      //     companyName: "company",
+      //     details: "details",
+      //   },
+      // ],
+      // capstone: {
+      //   name: "name",
+      //   description: "desc",
+      //   url: "url",
+      //   screenshot: "img",
+      // },
     };
   },
   created: function () {
@@ -59,24 +54,10 @@ export default {
   <div class="index">
     <div v-for="student in students" v-bind:key="student.firstName">
       <h1>Candidate Resumes</h1>
-      <h2>First Name: {{ student.firstName }}</h2>
-      <h2>Last Name: {{ student.lastName }}</h2>
-      <h3>Bio: {{ student.shortBio }}</h3>
-      <span>LinkedIn: {{ student.linkedIn }} | github: {{ student.github }}</span>
-      <br />
-      <a href="`/students/${student.id}`">Read more</a>
-
-      <h2>First Name: {{ student.firstName }}</h2>
-      <h2>Last Name: {{ student.lastName }}</h2>
-      <h3>Bio: {{ student.shortBio }}</h3>
-      <span>LinkedIn: {{ student.linkedIn }} | github: {{ student.github }}</span>
-      <br />
-      <a href="`/students/${student.id}`">Read more</a>
-
-      <h2>First Name: {{ student.firstName }}</h2>
-      <h2>Last Name: {{ student.lastName }}</h2>
-      <h3>Bio: {{ student.shortBio }}</h3>
-      <span>LinkedIn: {{ student.linkedIn }} | github: {{ student.github }}</span>
+      <h2>First Name: {{ student.first_name }}</h2>
+      <h2>Last Name: {{ student.last_name }}</h2>
+      <h3>Bio: {{ student.short_bio }}</h3>
+      <span>LinkedIn: {{ student.linkedin_url }} | github: {{ student.github_url }}</span>
       <br />
       <a href="`/students/${student.id}`">Read more</a>
     </div>
