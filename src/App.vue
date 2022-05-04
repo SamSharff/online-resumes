50ee91e41262662ff48f247d202f2eb5a9dd8032
deana <dvandegriff1@gmail.com>
2022-05-04 13:46:35 -0500

    adds NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/index">index</router-link>
     |
     <router-link to="/show">show</router-link>
     |
     <router-link to="/twitter">twitter</router-link>
   </nav>
+  -->
+  <!-- <router-view /> -->
+  -->
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"></a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">
+            Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/index">Resumes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/show">My Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/twitter">Twitter</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
   <router-view />
 </template>
 
@@ -17,16 +55,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
 }
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 nav a.router-link-exact-active {
   color: #42b983;
 }
